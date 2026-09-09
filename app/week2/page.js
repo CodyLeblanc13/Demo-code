2211446a9c00464ea2b00f9813d6b3ea9b2b21a4
CodyLeblanc13 <cody.leblanc@edu.sait.ca>
2026-09-09 09:54:17 -0600

    web dev week 2 demo commit
    demo followed in class and committed to a local repo

---
+++ app/week2/page.js
@@ -0,0 +1,10 @@
+import Title from "./title";
+export default function page() {
+  return (
+    <main>
+      <h1 className="text-3xl font-bold text-blue-600">Week 2 route/page</h1>
+      <p>more content</p>
+      <Title></Title>
+    </main>
+  );
+}
