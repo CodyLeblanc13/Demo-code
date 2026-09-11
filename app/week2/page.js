146f59c67131baaf89bd2f64fe06ab293cf0f264
CodyLeblanc13 <cody.leblanc@edu.sait.ca>
2026-09-11 15:16:45 -0600

    Small changes on page.js

--- app/week2/page.js
+++ app/week2/page.js
@@ -1,10 +1,13 @@
 import Title from "./title";
 export default function page() {
+  let a = 10;
+  let b = 20;
   return (
     <main>
       <h1 className="text-3xl font-bold text-blue-600">Week 2 route/page</h1>
       <p>more content</p>
-      <Title></Title>
+      <Title/>
+      <p>The sum of {a} + {b} is {a + b}</p>
     </main>
   );
 }
